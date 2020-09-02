0bd34175e82d9ef77934aad2d64756a838b6f3e3
Xuriwork <xuriwork@gmail.com>
2020-09-01 22:36:21 -0400

    added Alan AI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
 import Navbar from './components/Navbar/Navbar';
 import HomeContainer from './components/Home/HomeContainer';
@@ -9,6 +10,18 @@ import { StateProvider } from './context/StateContext';
 import { ModalProvider } from './context/ModalContext';
 
 const App = () => {
+	useEffect(() => {
+		const ALANAPIKEY = process.env.REACT_APP_ALAN_APIKEY;
+		alanBtn({
+			key: `${ALANAPIKEY}/stage`,
+			onCommand: ({ command }) => {
+				if (command === 'go:back') {
+					// Call the client code that will react to the received command
+				}
+			},
+		});
+	}, []);
+
 	return (
 		<StateProvider>
 			<ModalProvider>
